f5cecbf08ef3aac09ad1823c3b8be8b0fe771064
Lcole.engr <lcole.engr@gmail.com>
2024-03-03 20:30:28 -0500

    updated app so that conditional component is rendered with button clicks

--- src/components/ProjectsSidebar.jsx
+++ src/components/ProjectsSidebar.jsx
@@ -1,7 +1,7 @@
 import Button from "./Button"
 
 
-export default function ProjectsSidebar(){
+export default function ProjectsSidebar({onStartAddProject}){
 
     return(
         <aside className="w-1/3 px-8 py-16 bg-stone-900 text-stone-50 md:w-72 rounded-r-xl">
@@ -9,7 +9,7 @@ export default function ProjectsSidebar(){
             <h2 className="mb-8 font-bold uppercase md:text-xl text-stone-50">Your Projects</h2>
 
             <div>
-                <Button>
+                <Button onClick ={onStartAddProject}>
                      +  Add Project
                 </Button>
                  
